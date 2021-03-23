44b00508d2155d2f2b3dedda25123980683344ec
= <=>
2021-03-23 08:43:10 +0700

    first commit

---
+++ database/migrations/2021_03_23_012454_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->Nim(primarykey);
+            $table->Nama();
+            $table->kelas();
+            $table->Jurusan();
+            $table->No_Handphone();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
